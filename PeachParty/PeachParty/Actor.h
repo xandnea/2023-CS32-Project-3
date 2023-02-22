a90ab93625fab96e7d9e0b3a7ab7502bb6241d96
Xander Neary <xandnea@gmail.com>
2023-02-21 23:19:37 -0800

    Started Actor class

--- PeachParty/PeachParty/Actor.h
+++ PeachParty/PeachParty/Actor.h
@@ -5,4 +5,20 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class Actor : public GraphObject {
+public:
+	Actor(int X, int Y, int startDir); // need to set GraphObject IID, x, and y 
+
+	inline
+		bool isActive() { return m_active; } // check if object is active
+
+	virtual void doSomething() = 0; // prure virtual 
+
+	virtual ~Actor();
+
+private:
+	bool m_active;
+
+};
+
 #endif // ACTOR_H_
