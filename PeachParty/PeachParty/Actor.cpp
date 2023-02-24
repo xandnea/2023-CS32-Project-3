98c74c5459bbe3ceb91cdff2d7938cd4abf7db86
X_Laptop <xandnea@gmail.com>
2023-02-23 19:22:41 -0800

    Worked on PlayerAvatar implementation

--- PeachParty/PeachParty/Actor.cpp
+++ PeachParty/PeachParty/Actor.cpp
@@ -2,3 +2,77 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+// ACTOR IMPLEMENTATION
+Actor::Actor(StudentWorld* studentWorld, int imageID, int startX, int startY, int startDir, int depth) 
+	: GraphObject( imageID, startX, startY, startDir, depth, 1 )
+{
+	m_studentWorld = studentWorld;
+	m_active = true;
+}
+
+Actor::~Actor() { } // nothing dynamically allocated
+
+// PLAYER AVATAR IMPLEMENTATION
+PlayerAvatar::PlayerAvatar(StudentWorld* studentWorld, int player, int imageID, int startX, int startY)
+	: Actor(studentWorld, imageID, startX, startY, 0, 1 )
+{
+	m_player = player;
+	m_numCoins = 0;
+	m_numStars = 0;
+}
+
+void PlayerAvatar::swapCoins(PlayerAvatar& player) 
+{
+	int temp = player.getNumCoins();
+	player.setNumCoins(this->getNumCoins());
+	this->setNumCoins(temp);
+}
+
+void PlayerAvatar::swapPosAndState(PlayerAvatar& player)
+{
+	int tempX, tempY, tempState;
+
+	tempX = player.getX();
+	tempY = player.getY();
+	tempState = player.getState();
+
+	player.moveTo(this->getX(), this->getY());
+	player.setState(this->getState());
+
+	this->moveTo(tempX, tempY);
+	this->setState(tempState);
+}
+
+void PlayerAvatar::teleportPlayer(int x, int y)
+{
+	this->moveTo(x, y);
+}
+
+void PlayerAvatar::doSomething()
+{
+	int die_roll;
+
+	if (getState() == WAITING_TO_ROLL) {
+		// if the avatar has an invalid direction (due to being teleported):
+
+		int action = getWorld()->getAction(getPlayerNum());
+
+		if (action == ACTION_ROLL) {
+			die_roll = randInt(1, 10);
+			setTicks(die_roll * 8);
+			setState(WALKING);
+		}
+		// else if (action == ACTION_FIRE) {
+		//
+		// }
+		else { return; } // user didn't press a key or pressed another key
+	}
+	if (getState() == WALKING) {
+		// if the avatar is directly on top of a directional square
+		// else if the avatar is directly on top of a square at a fork 
+		if ()
+	}
+}
+
+PlayerAvatar::~PlayerAvatar() { } // idk what to delete
--- PeachParty/PeachParty/Actor.h
+++ PeachParty/PeachParty/Actor.h
@@ -2,23 +2,88 @@
 #define ACTOR_H_
 
 #include "GraphObject.h"
+#include "StudentWorld.h"
+
+const int WAITING_TO_ROLL = 0;
+const int WALKING = 1;
+
+const int PLAYER_ONE = 1;
+const int PLAYER_TWO = 2;
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
 class Actor : public GraphObject {
 public:
-	Actor(int X, int Y, int startDir); // need to set GraphObject IID, x, and y 
+	Actor(StudentWorld* studentWorld, int imageID, int startX, int startY, int startDir, int depth); // need to set GraphObject IID, x, and y 
 
 	inline
 		bool isActive() { return m_active; } // check if object is active
+	inline
+		void setActive(bool active) { m_active = active; } // setter
+	inline
+		StudentWorld* getWorld() { return m_studentWorld; } // return pointer to student world
 
-	virtual void doSomething() = 0; // prure virtual 
+	virtual void doSomething() = 0; // pure virtual 
 
 	virtual ~Actor();
 
 private:
 	bool m_active;
+	StudentWorld* m_studentWorld;
 
+};
+
+class PlayerAvatar : public Actor {
+public:
+	// Constructor
+	PlayerAvatar(StudentWorld* studentWorld, int player, int imageID, int startX, int startY);
+
+	// Getters
+	inline
+		int getPlayerNum() { return m_player; }
+	inline
+		int getNumCoins() { return m_numCoins; }
+	inline
+		int getNumStars() { return m_numStars; }
+	inline
+		bool checkVortex() { return m_vortex; }
+	inline
+		int getTicks() { return m_ticksToMove; }
+	inline
+		int getState() { return m_state; }
+	
+	// Setters
+	inline
+		void setNumCoins(int coins) { m_numCoins += coins; }
+	inline
+		void setNumStars(int stars) { m_numStars += stars; }
+	inline
+		void setVortex(bool vortex) { m_vortex = vortex; }
+	inline
+		void setTicks(int ticks) { m_ticksToMove = ticks; }
+	inline
+		void changeTicks(int ticks) { m_ticksToMove += ticks; }
+	inline
+		void setState(int state) { m_state = state; }
+
+	void swapCoins(PlayerAvatar& playerAvatar); // might want to use a pointer to playerAvatar instead
+	
+	void swapPosAndState(PlayerAvatar& playerAvatar);
+
+	void teleportPlayer(int x, int y);
+
+	void doSomething(); // no longer pure virtual
+
+	// Destructor
+	~PlayerAvatar();
+
+private:
+	int m_player;
+	int m_numCoins;
+	int m_numStars;
+	bool m_vortex;
+	int m_ticksToMove;
+	int m_state;
 };
 
 #endif // ACTOR_H_
